922cd6559daf990fe9ebf7623fbea0a57adfd21e
root <vikas.singla86@gmail.com>
2022-05-22 16:43:05 +0530

    added log in test_branch

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,6 +9,7 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts() {    
+    console.log("Fetch products");
     return this.http.get('http://localhost:3000/products');
   }
 
